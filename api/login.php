64e276fba02cb3178d988e67736496e3bd18bf51
olubanwo Ayomide <olubanwoayomide@gmail.com>
2021-08-26 16:50:26 +0100

    PHP

--- api/login.php
+++ api/login.php
@@ -1,3 +1,18 @@
 <?php
-echo "hello its me";
+    $con = mysqli_connect("localhost","root","root","webtrack");
+    if (isset($POST['submit'])) {
+        # code...
+        if (isset($POST['usr_uname']) && isset($POST['usr_psw'])) {
+            # code...
+            $name = $POST['usr_uname'];
+            $password = $POST['usr_psw'];
+
+            $query = "SELECT * FROM users WHERE name ='".$name."' and password='".$password."'";
+
+        }
+    } else {
+        # code...
+    }
+    
+    echo "hello its me";
 ?>
